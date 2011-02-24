32de41f7885d727d8b64cb2fe2010d3484f5c04b
Eliseu Massing <eliseujr@gmail.com>
2011-02-23 23:09:32 -0300

    First version of the Bovespa tracker application

---
+++ src/com/balk/bovespatracker/BovespaTracker.java
@@ -0,0 +1,13 @@
+package com.balk.bovespatracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BovespaTracker extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
