e4a223cb5fa8b951d4d87213f038293d5511e7b9
Eliseu Massing Jr <eliseujr@DarkStar.(none)>
2011-02-27 17:40:44 -0300

    Now downloading json file from finance.yahoo.com
    Next step is to parse the json file and show some data in the screen

--- src/com/balk/bovespatracker/BovespaTracker.java
+++ src/com/balk/bovespatracker/BovespaTracker.java
@@ -1,7 +1,18 @@
 package com.balk.bovespatracker;
 
+import com.balk.bovespatracker.StockDataHelper.ApiException;
+import com.balk.bovespatracker.StockDataHelper.ParseException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
 
 public class BovespaTracker extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +20,39 @@ public class BovespaTracker extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("teste Eliseu");
+
+        String pageContent = "";
+        
+        try {
+        	textView.setText("Inicio HTTP Connection");
+        	
+            StockDataHelper.prepareUserAgent(this);
+            pageContent = StockDataHelper.getPageContent();
+  
+            SAXParserFactory spfactory = SAXParserFactory.newInstance();
+            spfactory.setValidating(false);
+            SAXParser saxParser = spfactory.newSAXParser();
+            XMLReader xmlReader = saxParser.getXMLReader();
+            xmlReader.setContentHandler(new StockXmlReader());
+            xmlReader.setErrorHandler(new StockXmlReader());
+            InputSource source = new InputSource(pageContent);
+            xmlReader.parse(source);
+            
+            textView.setText("Fim HTTP Connection");
+        	textView.setText((CharSequence) pageContent);
+        } catch (ApiException e) {
+            Log.e("WordWidget", "Couldn't contact API", e);
+        } catch (ParseException e) {
+            Log.e("WordWidget", "Couldn't parse API response", e);
+        } catch (Exception e) {
+        	Log.e("WordWidget", "Final exception");
+        }        	
+        
+        
     }
+
 }
+
---
+++ src/com/balk/bovespatracker/StockXmlReader.java
@@ -0,0 +1,9 @@
+package com.balk.bovespatracker;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class StockXmlReader extends DefaultHandler {
+	
+	public StockXmlReader () { }
+
+}
